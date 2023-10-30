1a7f7592a94eff43045b722607713157cacb5034
Egor Grin <35704842+egorgrin@users.noreply.github.com>
2023-10-30 19:23:58 +0500

    slider is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,20 +13,24 @@
 
 }); */
 
-var swiper = new Swiper(".mySwiper", {
-  direction: 'horizontal',
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper2 = new Swiper(".mySwiper2", {
+const swiper = new Swiper(".mySwiper", {
   direction: 'horizontal',
   spaceBetween: 10,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+const swiper2 = new Swiper(".mySwiper2", {
+  direction: 'horizontal',
+  spaceBetween: 10,
+  /*navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },*/
   thumbs: {
     swiper: swiper,
   },
