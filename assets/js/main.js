80f071e080201487ea6c4935fde720e4f6b8a783
Egor Grin <35704842+egorgrin@users.noreply.github.com>
2023-10-30 19:08:52 +0500

    hero is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,27 @@
-const swiper = new Swiper(".mySwiper", {
+/* const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // autoHeight: true,
+  slidesPerView: 5,
+  slideToClickedSlide: true,
+  direction: 'horizontal',
+  // loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+}); */
+
+var swiper = new Swiper(".mySwiper", {
+  direction: 'horizontal',
   spaceBetween: 10,
   slidesPerView: 4,
   freeMode: true,
   watchSlidesProgress: true,
 });
-const swiper2 = new Swiper(".mySwiper2", {
+var swiper2 = new Swiper(".mySwiper2", {
+  direction: 'horizontal',
   spaceBetween: 10,
   navigation: {
     nextEl: ".swiper-button-next",
